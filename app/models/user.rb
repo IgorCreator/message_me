4589929bb20059465f1bf3824e80ecf83aac340a
Igor Asamadzinau <igor.asamadzinau@checkr.com>
2022-07-15 22:15:05 -0400

    Add Message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 15 }
   has_secure_password
+
+  has_many :messages
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "Evgeny", password: "pwd")
-User.create(username: "Jonsnow", password: "pwd")
-User.create(username: "Arya", password: "pwd")
-User.create(username: "Frodo", password: "pwd")
-User.create(username: "Gandalf", password: "pwd")
+u1 = User.create(username: "Evgeny", password: "pwd")
+u2 = User.create(username: "Jonsnow", password: "pwd")
+u3 = User.create(username: "Arya", password: "pwd")
+u4 = User.create(username: "Frodo", password: "pwd")
+u5 = User.create(username: "Gandalf", password: "pwd")
+
+m1 = Message.create(body: "Hi", user: u1)
+m2 = Message.create(body: "How are you?", user: u2)
+m3 = Message.create(body: "Hello World", user: u3)
+m4 = Message.create(body: "This messager too simple", user: u4)
